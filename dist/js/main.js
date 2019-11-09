054affc3d83e2bb7179c16e8dddd6ff41d11afc5
rjshoemaker55 <rjshoemaker55@gmail.com>
2019-11-09 12:28:10 -0500

    menu button animation

---
+++ dist/js/main.js
@@ -0,0 +1,32 @@
+// Select DOM Items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+// Set inital state of menu
+let showMenu = false;
+
+let toggleMenu = () => {
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItems.forEach(item => item.classList.add('show'));
+
+    // Set menu state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+
+    showMenu = false;
+  }
+};
+
+menuBtn.addEventListener('click', toggleMenu);
